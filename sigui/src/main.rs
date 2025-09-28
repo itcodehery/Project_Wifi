d8d4d758e62d3c8a5cbcfcec6a53f97e811ce1cb
Hari Prasad <haririo321@gmail.com>
2025-09-28 12:49:15 +0530

    added basic UI
    in process of learning how to use Ratatui as well

--- sigui/src/main.rs
+++ sigui/src/main.rs
@@ -1,18 +1,81 @@
+use color_eyre::eyre::{Ok, Result};
 use crossterm::event::{self, Event};
-use ratatui::{Frame, text::Text};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::{Constraint, Layout},
+    style::{Color, Stylize},
+    widgets::{Block, List, ListItem},
+};
 
-fn main() {
-    let mut terminal = ratatui::init();
-    loop {
-        terminal.draw(draw).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
-            break;
-        }
-    }
-    ratatui::restore();
+#[derive(Debug, Default)]
+struct AppState {
+    todo_list: Vec<TodoItem>,
 }
 
-fn draw(frame: &mut Frame) {
-    let text = Text::raw("Hello World!");
-    frame.render_widget(text, frame.area());
+#[derive(Debug, Default)]
+struct TodoItem {
+    is_done: bool,
+    description: String,
+}
+
+fn main() -> Result<()> {
+    let mut state = AppState::default();
+    state.todo_list.push(TodoItem {
+        is_done: false,
+        description: String::from("Learn Ratatui"),
+    });
+    state.todo_list.push(TodoItem {
+        is_done: false,
+        description: String::from("Mark the Attendance"),
+    });
+
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal, &mut state);
+
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal, state: &mut AppState) -> Result<()> {
+    loop {
+        // Rendering
+        terminal.draw(|frame| render(frame, state))?;
+        // Input handling
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                event::KeyCode::Esc => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(())
+}
+
+fn render(frame: &mut Frame, state: &AppState) {
+    let [border_area] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(frame.area());
+
+    let [inner_border] = Layout::vertical([Constraint::Fill(1)])
+        .margin(1)
+        .areas(border_area);
+
+    frame.render_widget(
+        Block::bordered()
+            .border_type(ratatui::widgets::BorderType::Rounded)
+            .fg(Color::Red),
+        frame.area(),
+    );
+
+    let list = List::new(
+        state
+            .todo_list
+            .iter()
+            .map(|x| ListItem::from(x.description.clone())),
+    );
+
+    frame.render_widget(list, inner_border);
 }
